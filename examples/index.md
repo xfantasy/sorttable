a41cec6379a1300651a520c55b995fa4b6132138
jiangbo.xi <jiangbo.xi@alipay.com>
2013-01-11 17:54:40 +0800

    =除中文排序外，其他ok

--- examples/index.md
+++ examples/index.md
@@ -1,4 +1,4 @@
-# 演示文档
+# 排序表格演示文档
 
 ---
 <style type="text/css">
@@ -22,11 +22,12 @@
 <table id='demo-table' class='sort-table'>
   <thead>
     <tr>
-      <th>标题</th>
-      <th>标题</th>
-      <th>标题</th>
-      <th>标题</th>
-      <th>标题</th>
+      <th>金额</th>
+      <th>数字</th>
+      <th>西文</th>
+      <th>符号</th>
+      <th>按卡号</th>
+      <th>中文</th>
     </tr>
   </thead>
   <tbody>
@@ -34,36 +35,41 @@
       <td>$ 4</td>
       <td>345,40.00000001</td>
       <td>arale</td>
-      <td>*</td>
-      <td>中文</td>
+      <td>****</td>
+      <td>CBC: <span class='card'>4309 7898 1298</span></td>
+      <td>變</td>
     </tr>
     <tr>
       <td>￥ 8,000</td>
       <td>123</td>
       <td>seajs</td>
       <td>^</td>
-      <td>排序</td>
+      <td>BBC: <span class='card'>4309 8898 1298</span></td>
+      <td>錒</td>
     </tr>
     <tr>
       <td>123 CNY</td>
       <td>123</td>
       <td>jquery</td>
       <td></td>
-      <td>是</td>
+      <td>ABC: <span class='card'>4309 9898 1298</span></td>
+      <td>畅</td>
     </tr>
     <tr>
       <td>89000.0</td>
       <td>1234</td>
       <td>YUI</td>
       <td>)</td>
-      <td>很</td>
+      <td>ICBC: <span class='card'>4309 1898 1298</span></td>
+      <td>看来</td>
     </tr>
     <tr>
       <td>-123,034.00</td>
       <td>345,40.001</td>
       <td>zepto</td>
       <td>#</td>
-      <td>麻烦的</td>
+      <td>BC: <span class='card'>5309 1898 1298</span></td>
+      <td>中文</td>
     </tr>
   </tbody>
  <tfoot>
@@ -76,8 +82,15 @@
 ````javascript
 seajs.use(['$', 'sorttable'], function($, Sorttable){
   a = new Sorttable({
-    element : '#demo-table'
+    element     : '#demo-table',
+    dataFilter  : function(td) {
+      // 自定义特殊字段的特殊取值方式
+      if ($(td).find('.card').length) {
+        return $(td).find('.card').text();
+      }
+      // 默认取整个td里的文本
+      return $(td).text();
+    }
   });
-  
 });
 ````
--- src/sorttable.js
+++ src/sorttable.js
@@ -1,27 +1,87 @@
 define(function(require, exports, module) {
   var Widget = require('arale/widget/1.0.2/widget-debug');
   var $ = require('$');
+  require('./sorttable.css');
+
+  // 常见货币符号
+  var CoinSymbol = '$ ￥ ￠ ￡ HKD CNY TWD EUR DEM GBP USD KPW JPY INR'.split(' ');
+
+  // 用于记录每一列的数据类型
+  var dataType = [];
+
+  // 当前排序的列次，以及正序/倒序状态当前排序状态
+  var orderStatus = {
+    'colIndex': -1,
+    'order': undefined
+  };  
 
   var Sorttable = Widget.extend({
-    attrs : {
-      trigger : {
-        value : '>thead>tr>th',
-        getter : function(val) {
+    attrs: {
+
+      // 触发器，默认为表头
+      'trigger': {
+        value: '>thead>tr>th',
+        getter: function(val) {
           return this.$(val);
         }
       },
-      rows : {
-        value : '>tbody:eq(0)>tr',
-        getter : function(val) {
+
+      // 需要排序的单元格
+      'rows': {
+        value: '>tbody:eq(0)>tr',
+        getter: function(val) {
           return this.$(val);
         }
+      },
+
+      // 正序的className
+      'ascClass': 'sort-table-asc',
+
+      // 倒序的className
+      'descClass': 'sort-table-desc',
+
+      // 过滤器，定义了从td中取那一部分数据来排序
+      'dataFilter': {
+        value: function(td) {
+          return $(td).text();
+        }
+      },
+
+      // 用于记录每一列的数据类型
+      'dataType': {
+        value: [],
+        geter: function() {
+          return dataType;
+        }
+      },
+    },
+
+    setup: function() {
+      Sorttable.superclass.setup.call(this);
+
+      // 将每一列的dataType记录下来
+      dataType = this.get('dataType');
+
+      // 所有行
+      var rows = this.get('rows');
+      // 得到每一列的dataType，为后面的排序做准备
+      for(var i = 0, l = rows.eq(0).children().length; i < l; i++) {
+        if(typeof dataType[i] !== 'string' || dataType[i] === '') {
+          dataType[i] = _detectType(_getColData(this, rows, i));
+        }
+      };
+
+      // 存在trigger时才绑定click事件
+      if(this.get('trigger').length) {
+        _bindSortEvent(this);
       }
     },
-    setup : function () {
-      var that = this;
-      Sorttable.superclass.setup.call(this);
-      _bindSortEvent(this);
+
+    // 返回表格的排序状态
+    getOrderStatus: function() {
+      return orderStatus;
     },
+
     /**
      * 排序
      * 1. 如果order不传，或为空，则自动排序/倒序
@@ -29,136 +89,174 @@ define(function(require, exports, module) {
      * @param {number} colIndex   按第(column)列排序
      * @param {string} order    排序方向，可选
      */
-    sort : function(colIndex, order) {
+    sort: function(colIndex, order) {
+      var trigger = this.get('trigger');
 
-    },
+      // 得到所有行
+      var rows = _getRows(this);
+      var colCount = $(rows).eq(0).children().length - 1;
+      var colIndex = colIndex || 0;
+      colIndex = colIndex < 0 ? 0 : (colIndex > colCount ? colCount : colIndex);
 
-    
-  });
+      // 如果当前需要排序的列已经是有序态，且跟要排序的方向一致，则不用排啦
+      if(colIndex == orderStatus.colIndex && orderStatus.order == order) return orderStatus;
 
-  // 常见货币符号
-  var CoinSymbol = '$ ￥ ￠ ￡ HKD CNY TWD EUR DEM GBP USD KPW JPY INR'.split(' ');
+      // 得到所有列的数据
+      var colData = _getColData(this, rows, colIndex);
 
-  /**
-   * 鼠标移动事件
-   * @return {[type]} [description]
-   */
-  var _bindMouseOverEvent = function() {
-  };
-
-  var _getData = function(text, type) {
-    var data = text;
-    switch(type) {
-      case 'coin':
-        for (var i = 0, l = CoinSymbol.length; i < l; i++) {
-          data = data.replace(CoinSymbol[i], '');
-        }
-      case 'coin':
-      case 'number':
-        data = data.trim().replace(/[,]/g, '') * 1
-        break;
-      default:
-    }
-    return data;
-  };
-
-
-  /**
-   * 判断数据类型
-   * @param  {string} str 传入的html片段、或者数据
-   * @return {string}     数据类型
-   */
-  var _detectType = function(str) {
-    // 空节点
-    if (str.trim() === '') return 'other';
-    for (var i = 0, l = CoinSymbol.length; i < l; i++) {
-      // 首尾有货币符号时，则定义为货币
-      if (str.indexOf(CoinSymbol[i]) === 0 ||
-          (str.indexOf(CoinSymbol[i]) > 0) && (str.indexOf(CoinSymbol[i]) === str.length - CoinSymbol[i].length)) {
-        return 'coin';
+      // 倒序
+      if(colIndex == orderStatus.colIndex) {
+        var orderData = colData.reverse();
+        orderStatus.order = orderStatus.order == 'desc' ? 'asc' : 'desc';
+      // 排序
+      } else {
+        var orderData = _sort(colData, dataType[colIndex]);
+        orderData = order == 'desc' ? orderData.reverse() : orderData;
+        orderStatus.order = order == 'desc' ? 'desc' : 'asc'; //默认正序
+        orderStatus.colIndex = colIndex;
       }
-    }
-    // 数字类型检测
-    // TODO: 兼容科学计数法
-    if (/^([-]*\d{1,3}(,\d{3})*(\.\d+)?|\d+(\.\d+)?)$/.test(str)) return 'number';
-    // 西文字母&标点(如果字母数字标点占比大于50%，则认为这是一个字符串)
-    if (str.replace(/[^a-zA-Z0-9,.!?:;\ ]/g, '').length / str.length > 0.5) return 'character';
-    // 中文
-    if (/[\u4E00-\u9FA5\uF900-\uFA2D]/.test(str)) return 'chinese';
-    return 'other';
-  };
 
-  /**
-   * 排序
-   * @param {number} data     需要排序的数据
-   * @param {string} order    排序方向，可选
-   */
-  //!!! 如果当前列已经是asc/desc状态，则只需要倒序或不动即可
-  var _sort = function(data, order) {
-    // eg: data = {str : '原始字符串', data : '格式化后的数据', index : '在表格中的行数', type : '数据类型'}
-    var types = [];
-    $(data).each(function() {
-      types.push(this.type = _detectType(this.str));
-      this.data = _getData(this.str, this.type);
-    });
-    // 排序规则： other/character > chinese > coin/number
-    if (types.indexOf('other') != -1 || types.indexOf('character') != -1 ) {
-      data.sort(function(a, b) {
-        return (a.data + '').localeCompare(b.data + '');
-      });
-    } else if (types.indexOf('chinese') != -1 ) {
-      data.sort(function(a, b) {
-        return (a.data + '').localeCompare(b.data + '');
-      });
-    } else if (types.indexOf('coin') != -1 || types.indexOf('number') != -1 ) {
-      data.sort(function(a, b) {
-        return a.data > b.data;
-      });
-    }
-    return data;
-  };
+      // 给triggers设置样式
+      if(trigger.length) {
+        $(trigger).removeClass(this.get('descClass') + ' ' + this.get('ascClass'));
+        $(trigger).eq(colIndex).addClass(orderStatus.order == 'asc' ? this.get('ascClass') : this.get('descClass'));
+      }
+
+      var html = _sortRow(rows, orderData);
+      rows.remove();
+      this.element.find('tbody:eq(0)').append(html);
+      return orderStatus;
+    },
+  });
 
   /**
    * 绑定th上的排序事件
-   * @param  {[type]} that [description]
-   * @return {[type]}      [description]
    */
   var _bindSortEvent = function(that) {
     var trigger = that.get('trigger');
-    $(trigger).bind('click', function (e) {
+    $(trigger).bind('click', function(e) {
+      var currentTarget = $(e.currentTarget);
       var colIndex = -1;
-      for (var i = 0, l = trigger.length; i < l; i++){
-        if (trigger[i] == e.currentTarget){
+      for(var i = 0, l = trigger.length; i < l; i++) {
+        if(trigger[i] == e.currentTarget) {
           colIndex = i;
           break;
         }
       }
-      var rows = _getRows(that.element);          //得到所有列
-      var colData = _getColData(rows, colIndex);  //得到所有列的数据
-      var order = $(e.currentTarget).data('order') == 'undefined' ?
-        'asc' : ($(e.currentTarget).data('order') == 'asc' ? 'desc' : 'asc');
-      var sortOrder = _sort(colData, order);      //将数据排序后的index顺序返回
-      _sortRow(that, sortOrder);                        //排序表格
+      return that.sort(colIndex, $(e.currentTarget).data('order'));
     });
   };
 
   /**
-   * 得到所有的行
-   * @return {[type]} [description]
+   * 转换文本为对应数据
+   * @param  {string} text 需要转换的文本
+   * @param  {string} type 需要转换的类型
+   * @return {string} 转换后的数据
    */
-  var _getRows = function (table) {
-    return $(table).find('tbody:eq(0)>tr');
+  var _convertData = function(text, type) {
+    var data = text;
+    switch(type) {
+    case 'coin':
+      for(var i = 0, l = CoinSymbol.length; i < l; i++) {
+        data = data.replace(CoinSymbol[i], '');
+      }
+    case 'coin':
+    case 'number':
+      data = data.trim().replace(/[,]/g, '') * 1
+      break;
+    default:
+    }
+    return data;
   };
 
   /**
-   * 得到对应列的数据
-   * @return {[type]} [description]
+   * 判断数据类型(给出一组数据，跟据不同元素的类型，决定这一组元素的总体类型)
+   * @param  {string} str 传入的html片段、或者数据
+   * @return {string}     数据类型
    */
-  var _getColData = function(rows, colIndex) {
+  var _detectType = function(data, target) {
+    var types = [];
+    // 类型检测
+    var detectType = function(str) {
+        if(str.trim() === '') return 'other';
+        for(var i = 0, l = CoinSymbol.length; i < l; i++) {
+          // 首尾有货币符号时，则定义为货币
+          if(str.indexOf(CoinSymbol[i]) === 0 || (str.indexOf(CoinSymbol[i]) > 0) && (str.indexOf(CoinSymbol[i]) === str.length - CoinSymbol[i].length)) {
+            return 'coin';
+          }
+        }
+        // 数字类型检测( 千分位记数法 || 普通数字和科学记数法 )
+        if(/^([-]*\d{1,3}(,\d{3})*(\.\d+)?|\d+(\.\d+)?)$/.test(str) || str * 1 === str * 1) return 'number';
+        // 中文(首字母为中文，则为中文)
+        if(/^[\u4E00-\u9FA5\uF900-\uFA2D]/ig.test(str)) return 'chinese';
+
+        // 西文字母&标点(如果字母数字标点占比大于50%，则认为这是一个字符串)
+        // TODO：更精确的西文字符检测方法
+        // if (str.replace(/[^a-zA-Z0-9,.!?:;\ ]/g, '').length / str.length > 0.5) return 'character';
+        return 'character';
+      };
+    for(var i = 0, l = data.length; i < l; i++) {
+      types.push(detectType(data[i].str.trim()));
+    }
+    // 返回值，根据types数组中的类型组合决定
+    if(types.indexOf('other') != -1 || types.indexOf('character') != -1) {
+      return 'character';
+    } else if(types.indexOf('chinese') != -1) {
+      return 'chinese';
+    } else if(types.indexOf('coin') != -1) {
+      return 'coin';
+    } else if(types.indexOf('number') != -1) {
+      return 'number';
+    }
+  };
+
+  /**
+   * 排序函数
+   * @param  {array}  data     [需要排序的数组]
+   * @param  {string} dataType [数据类型]
+   * @return {array}           [排序后的数据]
+   */
+  var _sort = function(data, dataType) {
+    // 按指定的数据类型处理原始数据
+    $(data).each(function() {
+      this.data = _convertData(this.str, dataType);
+    });
+    // 按指定的数据类型排序
+    if(dataType === 'character') {
+      data.sort(function(a, b) {
+        return(a.data + '').localeCompare(b.data + '');
+      });
+    } else if(dataType === 'chinese') {
+      data.sort(function(a, b) {
+        return(a.data + '').localeCompare(b.data + '');
+      });
+    } else if(dataType === 'number' || dataType === 'coin') {
+      data.sort(function(a, b) {
+        return a.data * 1 > b.data * 1;
+      });
+    }
+    return data;
+  };
+
+  /**
+   * 得到所有需要排序的行
+   */
+  var _getRows = function(that) {
+    return $(that.element).find(that.get('rows'));
+  };
+
+  /**
+   * 得到对应单元格的数据
+   * @param  {object} that     Sorttable
+   * @param  {DOM} rows     所有的行
+   * @param  {number} colIndex 列索引
+   * @return {string}  返回单元格里的文本，或由dataFilter过滤后的文本
+   */
+  var _getColData = function(that, rows, colIndex) {
     var colData = [];
     $(rows).each(function(index) {
       colData.push({
-        str   : $(this).children().eq(colIndex).text().trim(),
+        str   : that.get('dataFilter')($(this).children().eq(colIndex)),
         index : index //记录现在的index值，用于排序
       });
     });
@@ -167,16 +265,16 @@ define(function(require, exports, module) {
 
   /**
    * 移动行
-   * @return {[type]} [description]
+   * @param  {DOM} rows  排序前的DOM
+   * @param  {array} data 排序后的HTML
+   * @return {HTML} 排序后的html
    */
-  // TODO: 效率问题
-  var _sortRow = function (that, data) {
+  var _sortRow = function(rows, data) {
     var newTbody = [];
     $(data).each(function() {
-      newTbody.push(that.get('rows').eq(this.index)[0].outerHTML);
+      newTbody.push($(rows).eq(this.index)[0].outerHTML);
     });
-    that.element.find('tbody:eq(0)>tr').remove();
-    that.element.find('tbody:eq(0)').append(newTbody.join(' '));
+    return newTbody.join(' ');
   };
 
   module.exports = Sorttable;
